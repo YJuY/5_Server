75a0c1ca5a176c6c3bc425723735d085e6c157b7
YjuY <sd05sd@naver.com>
2022-10-13 18:26:44 +0900

    2022.10.13

---
+++ JSPProject2/src/main/java/edu/kh/jsp/model/vo/Person.java
@@ -0,0 +1,45 @@
+package edu.kh.jsp.model.vo;
+
+public class Person {
+	// 필드
+	private String name;	// 왜 private? 캡슐화 원직에 의해(필드에 직접 접근 제한)
+	private int age;
+	private String address;
+	
+	// 컨트롤 스페이스 엔터
+	public Person() { } // 기본 생성자
+
+	// getter/setter
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	// Object.toString() 오버라이딩
+	@Override
+	public String toString(){
+		return name+" / " + age + " / " + address;
+	}
+	
+	
+	
+}
